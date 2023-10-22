ffaaa8fae76ed0ab46adad04f1eb61a0baec9e25
m2600 <shota.M.020626.S.K.F@gmail.com>
2023-10-22 09:18:29 +0900

    テスト用キーマップ配列とキーマップ配列のチェックメソッドのバグ修正

--- main.py
+++ main.py
@@ -1,6 +1,128 @@
 
 
-'''keyMap = [
+NONE = 'NONE'
+____ = '____'
+KEY_ESCP = 'KEY_ESCP'
+KEY_FU1 = 'KEY_FU1'
+KEY_FU2 = 'KEY_FU2'
+KEY_FU3 = 'KEY_FU3'
+KEY_FU4 = 'KEY_FU4'
+KEY_FU5 = 'KEY_FU5'
+KEY_FU6 = 'KEY_FU6'
+KEY_GRV = 'KEY_GRV'
+KEY_1 = 'KEY_1'
+KEY_2 = 'KEY_2'
+KEY_3 = 'KEY_3'
+KEY_4 = 'KEY_4'
+KEY_5 = 'KEY_5'
+KEY_6 = 'KEY_6'
+KEY_TB = 'KEY_TB'
+KEY_Q = 'KEY_Q'
+KEY_W = 'KEY_W'
+KEY_E = 'KEY_E'
+KEY_R = 'KEY_R'
+KEY_T = 'KEY_T'
+KEY_LPRN = 'KEY_LPRN'
+KEY_CAPS = 'KEY_CAPS'
+KEY_A = 'KEY_A'
+KEY_S = 'KEY_S'
+KEY_D = 'KEY_D'
+KEY_F = 'KEY_F'
+KEY_G = 'KEY_G'
+KEY_LCBR = 'KEY_LCBR'
+KEY_LSFT = 'KEY_LSFT'
+KEY_Z = 'KEY_Z'
+KEY_X = 'KEY_X'
+KEY_C = 'KEY_C'
+KEY_V = 'KEY_V'
+KEY_B = 'KEY_B'
+KEY_LCTL = 'KEY_LCTL'
+KEY_LGUI = 'KEY_LGUI'
+KEY_FN = 'KEY_FN'
+KEY_LALT = 'KEY_LALT'
+KEY_RAIS = 'KEY_RAIS'
+KEY_RSFT = 'KEY_RSFT'
+KEY_DEL = 'KEY_DEL'
+KEY_SPC = 'KEY_SPC'
+KEY_MUTE = 'KEY_MUTE'
+KEY_VOLUMEDOWN = 'KEY_VOLUMEDOWN'
+KEY_VOLUMEUP = 'KEY_VOLUMEUP'
+KEY_CPFL = 'KEY_CPFL'
+KEY_CLPF = 'KEY_CLPF'
+KEY_ARLS = 'KEY_ARLS'
+KEY_KLCK = 'KEY_KLCK'
+KEY_BSPC = 'KEY_BSPC'
+KEY_BSLS = 'KEY_BSLS'
+KEY_7 = 'KEY_7'
+KEY_8 = 'KEY_8'
+KEY_9 = 'KEY_9'
+KEY_0 = 'KEY_0'
+KEY_MINS = 'KEY_MINS'
+KEY_EQL = 'KEY_EQL'
+KEY_RPRN = 'KEY_RPRN'
+KEY_Y = 'KEY_Y'
+KEY_U = 'KEY_U'
+KEY_I = 'KEY_I'
+KEY_O = 'KEY_O'
+KEY_P = 'KEY_P'
+KEY_LBRC = 'KEY_LBRC'
+KEY_RBRC = 'KEY_RBRC'
+KEY_RCBR = 'KEY_RCBR'
+KEY_H = 'KEY_H'
+KEY_J = 'KEY_J'
+KEY_K = 'KEY_K'
+KEY_L = 'KEY_L'
+KEY_MUP = 'KEY_MUP'
+KEY_MDWN = 'KEY_MDWN'
+KEY_MRIT = 'KEY_MRIT'
+KEY_MLFT = 'KEY_MLFT'
+KEY_MLCL = 'KEY_MLCL'
+KEY_MHUP = 'KEY_MHUP'
+KEY_MHDW = 'KEY_MHDW'
+KEY_MRCL = 'KEY_MRCL'
+KEY_M = 'KEY_M'
+KEY_FU7 = 'KEY_FU7'
+KEY_FU8 = 'KEY_FU8'
+KEY_FU9 = 'KEY_FU9'
+KEY_FU10 = 'KEY_FU10'
+KEY_FU11 = 'KEY_FU11'
+KEY_FU12 = 'KEY_FU12'
+KEY_KLCK = 'KEY_KLCK'
+KEY_CPFL = 'KEY_CPFL'
+KEY_HOM = 'KEY_HOM'
+KEY_INS = 'KEY_INS'
+KEY_PSCR = 'KEY_PSCR'
+KEY_PGUP = 'KEY_PGUP'
+KEY_PGDN = 'KEY_PGDN'
+KEY_MAIL = 'KEY_MAIL'
+KEY_COMM = 'KEY_COMM'
+KEY_DOT = 'KEY_DOT'
+KEY_SCLN = 'KEY_SCLN'
+KEY_QUOT = 'KEY_QUOT'
+KEY_UP = 'KEY_UP'
+KEY_ENT = 'KEY_ENT'
+KEY_N = 'KEY_N'
+KEY_SLSH = 'KEY_SLSH'
+KEY_LEFT = 'KEY_LEFT'
+KEY_DOWN = 'KEY_DOWN'
+KEY_RGHT = 'KEY_RGHT'
+KEY_LOWE = 'KEY_LOWE'
+KEY_RALT = 'KEY_RALT'
+KEY_EN = 'KEY_EN'
+KEY_KEYPAD_0 = 'KEY_KEYPAD_0'
+KEY_KEYPAD_1 = 'KEY_KEYPAD_1'
+KEY_KEYPAD_2 = 'KEY_KEYPAD_2'
+KEY_KEYPAD_3 = 'KEY_KEYPAD_3'
+KEY_KEYPAD_4 = 'KEY_KEYPAD_4'
+KEY_KEYPAD_5 = 'KEY_KEYPAD_5'
+KEY_KEYPAD_6 = 'KEY_KEYPAD_6'
+KEY_KEYPAD_7 = 'KEY_KEYPAD_7'
+KEY_KEYPAD_8 = 'KEY_KEYPAD_8'
+KEY_KEYPAD_9 = 'KEY_KEYPAD_9'
+
+
+
+testKeyMap = [
     # left
     [KEY_ESCP, KEY_FU1, KEY_FU2, KEY_FU3, KEY_FU4, KEY_FU5, KEY_FU6, NONE],
     [KEY_GRV, KEY_1, KEY_2, KEY_3, KEY_4, KEY_5, KEY_6, NONE],
@@ -216,7 +338,7 @@
         NONE,
         NONE,
         NONE,
-    ]]'''
+    ]]
 
 
 
@@ -229,6 +351,7 @@ class keymap_generator:
 
     def __init__(self):
         self.keymapTemplata_dir = 'keymap_template'
+        self.keymapFile_dir = 'keymap.h'
         self.keymapRow = 7 * 16  # 7 rows each 16 profiles
         self.keymapCol = 8       # 8 columns    
         # Read keymap template
@@ -242,14 +365,20 @@ class keymap_generator:
             print('keymap row size error')
             return
         else:
-            for row in keymap:
-                if isinstance(row, list) or len(row) != self.keymapCol:
+            for col in keymap:
+                if len(col) != self.keymapCol:
                     print('keymap column size error')
                     return
+                else:
+                    for key in col:
+                        if isinstance(key, list):
+                            print('Keymap dimension is too high. It must be 2D array.')
+                            return
         # Generate keymap file
         keymap_text = self.keymapTemplate + self.generate_cpp_array_from_python_array('const byte keyMap', keymap)
-        with open('keymap.h', 'w') as f:
+        with open(self.keymapFile_dir, 'w') as f:
             f.write(keymap_text)
+        print(self.keymapFile_dir + ' generated.')
 
     def generate_cpp_array_from_python_array(self, arrayName, keymap):
         self.cpp_array = ''
@@ -278,16 +407,18 @@ class keymap_generator:
         self.cpp_array += '},\n'
 
 
-arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
 def main():
     generater = keymap_generator()
-    generater.generate_keymap_file(arr)
+    generater.generate_keymap_file(testKeyMap)
 
     if os.name == 'nt':
         arduinoCli_path = 'arduino-cli/arduino-cli.exe'
     elif os.name == 'posix':
         arduinoCli_path = 'arduino-cli'
+    else:
+        print('OS not supported')
+        return
     arduino = pyduinocli.Arduino(arduinoCli_path)
     bords = arduino.board.list()
     print(bords)
