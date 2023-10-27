3c61b0c267c331cc2000a4f248b444e932f8e828
M260 <41881281+M2600@users.noreply.github.com>
2023-10-27 12:14:43 +0900

    eelとりあえず

---
+++ web.py
@@ -0,0 +1,13 @@
+
+import eel
+
+
+def main():
+    eel.init('web')
+    # Open in default browser
+    #eel.start('main.html', mode='default', size=(600, 400))
+    eel.start('main.html', size=(600, 400))
+
+
+if __name__ == '__main__':
+    main()
